9cb1569b981a25155bbd382a1bd6f8d200c47cd4
Navdeep <navdeepsingh8847@outlook.com>
2025-12-24 19:36:21 +0530

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+// alert("Hello"); // we are using node.js not browser 
+
+console.log(3+3) 
+console.log("Hello") //semicolon should only be used when writing 2 or more codes in same line but we generally dont do that for readability
+ 
+let name = "Pengu"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53 (Range of number vairable)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => not assigned value
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+

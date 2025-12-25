41725e957ac4486738268d1d261a3270bca26896
Navdeep <navdeepsingh8847@outlook.com>
2025-12-25 09:58:02 +0530

    conversionoperation

---
+++ 01basics/03conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "pengu"
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 33
+let valueInBoolean = Boolean(isLoggedIn)
+console.log(valueInBoolean);
+console.log(typeof valueInBoolean);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "pengu" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

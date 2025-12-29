683bab6733ff3ac325e54408895dcd0578b82d99
Navdeep <navdeepsingh8847@outlook.com>
2025-12-29 08:53:27 +0530

    date-finish

--- 01basics/07datesInJs.js
+++ 01basics/07datesInJs.js
@@ -10,6 +10,21 @@ console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23) // Months start from zero in JS
 // let myCreatedDate = new Date(2023, 0, 23, 5 ,3)
-let myCreatedDate = new Date("01-14-2025")
+let myCreatedDate = new Date("01-14-2025") 
+console.log(myCreatedDate.toLocaleString('en-IN'));
 
-console.log(myCreatedDate.toLocaleString());
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+
+// Important
+console.log(newDate.toLocaleString('default', {
+  weekday: "long",
+}));

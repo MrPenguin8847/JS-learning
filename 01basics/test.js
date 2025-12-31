8540bf537295ec399e50e4311ed6d22165c2ab17
Navdeep <navdeepsingh8847@outlook.com>
2025-12-31 22:36:42 +0530

    objects start

--- 01basics/test.js
+++ 01basics/test.js
@@ -4,3 +4,18 @@ const regex1 = /the/gi // a regular expression but gi defines that it won't care
 const regex2 = 'the' // match will automatically convert this string into a regular expression.
 const found = paragraph1.match(regex)
 console.log(found);
+
+
+let myobj = {
+  name: "pengu",
+  age: 22,
+}
+
+console.log(typeof myobj);
+
+const heros = ["shaktiman", "naagraj", "doga"]
+console.log(typeof heros);
+
+
+const marvelHeros = ["Thor", "IronMan", "SpiderMan"]
+console.log(typeof marvelHeros);
---
+++ 02basics/03objects.js
@@ -0,0 +1,26 @@
+// Singleton =>  If we declare literal toh singleton nahi bnta hai only constructor declare krne par singleton bnn skta hai
+
+// Object literals
+
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+  "full name": "Pengu",
+  age: 18,
+  [mySym]: "mykey1", // syntax to use Symbol
+  location: "Jaipur",
+  email: "Pengu@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"]
+}
+
+
+console.log(JsUser.email);
+console.log(JsUser["email"]); // This syntax is main and effective as the upper syntax has some flaws
+// console.log(JsUser.full name); // When the string name has space it fails and gives error
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+console.log(typeof JsUser[mySym]);
+
+JsUser.email = "Pengu@chatgpt.com"

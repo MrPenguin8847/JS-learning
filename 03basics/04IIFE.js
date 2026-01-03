f653add4f9d5cce012d4a7209847895e32414fcb
Navdeep <navdeepsingh8847@outlook.com>
2026-01-03 10:54:16 +0530

    04IIFE

---
+++ 03basics/04IIFE.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expression (IIFE) => Its a function which gets executed immidiately and 
+// Why it exists:
+// 1. Avoids polluting the global scope
+// 2. Keeps variables private
+// 3. Runs setup code once
+
+(function chai(){ //name IIFE
+  console.log(`DB Connected`);
+})(); // This is the synatx of IIFE and the semicolon at the end is very important as it ends the IIFE function
+
+( (name) => { // UnNamed IIFE, and parameters can be passed like normal
+  console.log(`DB Connected 2 ${name}`);
+})(`hitesh`);

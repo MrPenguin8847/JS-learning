7b7da06d402036ab888723086ae1b633888d4773
Navdeep <navdeepsingh8847@outlook.com>
2026-01-05 22:56:13 +0530

    iteration1

---
+++ 05Iterations/1.js
@@ -0,0 +1,27 @@
+// Iterations
+// for loop
+for (let i = 0; i < 10; i++) {
+  const element = i;
+  if (element == 5) {
+    console.log("5 is best number")
+  }
+  console.log(element)
+}
+
+for (let i = 1; i <= 10; i++) {
+  const element = i;
+  console.log(`Outer Loop value: ${i}`);
+  for (let j = 1; j < 10; j++) {
+    const element = j;
+    console.log(`${j}${i}`);
+    console.log(i + '*' + j + '=' + i * j);
+  }
+}
+let myArray = ["flash", "batman", 'superman']
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
+
+// 

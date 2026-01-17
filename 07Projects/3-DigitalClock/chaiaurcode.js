2f760a7b7441eb78b9d3471e625255afa52e0f61
Navdeep <navdeepsingh8847@outlook.com>
2026-01-17 10:41:36 +0530

    Making-projects-again

--- 07Projects/3-DigitalClock/chaiaurcode.js
+++
@@ -1,8 +0,0 @@
-const clock = document.getElementById('clock')
-
-// To create a date which changes we use setInterval
-setInterval(() => {
-    let date = new Date()
-    // console.log(date.toLocaleTimeString);
-    clock.innerHTML = date.toLocaleTimeString()
-}, 1000);
